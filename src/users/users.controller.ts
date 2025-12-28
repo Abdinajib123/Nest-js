7051a7218f3afb263f9d5582d29911f44f857730
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2025-12-28 10:10:08 +0300

    module,controller,service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    findAll() {
+        return [
+            { id: 1, name: 'Ahmed' },
+            { id: 2, name: 'Ali' },
+        ];
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+}
