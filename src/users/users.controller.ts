b3d6eaf45bfef7620f5e2657d440dd3e0a183f15
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2025-12-29 10:02:08 +0300

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
-    @Get()
-    findAll() {
-        return [
-            { id: 1, name: 'Ahmed' },
-            { id: 2, name: 'Ali' },
-        ];
-    }
+ @Get()
+ findAll(){
+    return[{id:1,name:"Abdinajib"}]
+ }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return { id };
-    }
+ @Get(':id')
+findOne(@Param('id') id: string){
+    return {id:id,name:"Abdinajib"}
+ }
+
+ @Post()
+ create(@Body() user:{}){
+    return user
+ }
+ @Patch(':id')
+ update(@Param('id') id:string, @Body() userupde:{}){
+    return {id,...userupde}
+ }
+ @Delete(':id')
+ remove(@Param('id') id:string){
+    return {msg:`user with id ${id} deleted`}
+ }
+
 }
